403f7bc89844347e03120ab110e889d1469ef572
chandraTI24F <chandrabagza133@gmail.com>
2026-06-06 19:58:04 +0700

    Add files via upload

---
+++ Percobaan6.java
@@ -0,0 +1,21 @@
+public class Percobaan6 {
+
+    static void demo() {
+        NullPointerException t;
+        t = new NullPointerException("Coba Throw");
+        throw t;
+
+        // Baris ini tidak akan dijalankan
+        // System.out.println("Ini tidak lagi dicetak");
+    }
+
+    public static void main(String[] args) {
+        try {
+            demo();
+            System.out.println("Selesai");
+        }
+        catch(NullPointerException e) {
+            System.out.println("Ada pesan error: " + e);
+        }
+    }
+}
